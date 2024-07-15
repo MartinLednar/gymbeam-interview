d3e033a70ce5350373c6a69f74ddd49e572b519b
MartinLednar <martin.lednar03@gmail.com>
2024-07-15 23:25:09 +0200

    feat(gymbeam-1): added dark mode

--- src/app/lists/[listId]/page.tsx
+++ src/app/lists/[listId]/page.tsx
@@ -2,14 +2,29 @@
 
 import { CreateTodoForm } from "@/components/forms/todo/createTodo.form";
 import { Todo } from "@/components/todo/todo.component";
+import { useDeleteList } from "@/hooks/lists/mutations/useDeleteList.hook";
 import { useGetList } from "@/hooks/lists/queries/useGetList/useGetList.hook";
+import { calcDoneTodos } from "@/utils/todos.util";
 import { CircleCheckBig, Trash2 } from "lucide-react";
 
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+import { useMemo } from "react";
 
 const ListPage = () => {
   const { listId } = useParams<{ listId: string }>();
+  const { push } = useRouter();
   const { isLoading, isSuccess, isError, data } = useGetList({ listId });
+  const { mutateAsync } = useDeleteList();
+
+  const filteredTodos = useMemo(
+    () => (data ? calcDoneTodos(data?.totalTodos) : null),
+    [data]
+  );
+
+  const handleDeleteList = async () => {
+    await mutateAsync({ id: listId });
+    push("/");
+  };
 
   return (
     <div className="container mx-auto py-10 px-4">
@@ -21,6 +36,7 @@ const ListPage = () => {
               <p>Created: {new Date(data.createdAt).toLocaleDateString()}</p>
             </div>
             <button
+              onClick={handleDeleteList}
               type="button"
               className="flex gap-x-1 p-2 ml-auto bg-red-600 text-white rounded-md"
             >
@@ -32,8 +48,8 @@ const ListPage = () => {
           <div className="max-w-xl  mx-auto pt-20">
             <h2 className="text-2xl font-semibold pb-6">To-Do</h2>
             <div className="max-h-[50svh] overflow-y-scroll flex flex-col gap-y-3">
-              {data.totalTodos.length > 0
-                ? data.totalTodos.map((todo) => (
+              {filteredTodos && filteredTodos.undoneTodos.total > 0
+                ? filteredTodos.undoneTodos.todos.map((todo) => (
                     <Todo key={todo.id} {...todo} />
                   ))
                 : null}
@@ -44,8 +60,8 @@ const ListPage = () => {
           <div className="max-w-2xl max-h-[50svh] overflow-y-scroll flex flex-col gap-y-3 mx-auto pt-20">
             <h2 className="text-2xl font-semibold pb-6">Completed tasks</h2>
             <div className="max-h-[50svh] overflow-y-scroll flex flex-col gap-y-3">
-              {data.totalTodos.length > 0
-                ? data.totalTodos.map((todo) => (
+              {filteredTodos && filteredTodos.doneTodos.total > 0
+                ? filteredTodos.doneTodos.todos.map((todo) => (
                     <Todo key={todo.id} {...todo} />
                   ))
                 : null}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,16 @@ const HomePage = () => {
 
   return (
     <>
-      <CreateListForm />
-
-      <main className="grid grid-cols-3 gap-x-3 gap-y-3 p-10">
-        {isSuccess
-          ? data.map((list) => <ListPreview key={list.id} listData={list} />)
-          : null}
+      <main className="container px-5 mx-auto h-svh">
+        <div className="flex items-center justify-between pb-10 pt-20">
+          <h1 className=" font-bold text-4xl">My To-Dos</h1>
+          <CreateListForm />
+        </div>
+        <div className="grid grid-cols-3 gap-x-3 gap-y-3">
+          {isSuccess
+            ? data.map((list) => <ListPreview key={list.id} listData={list} />)
+            : null}
+        </div>
       </main>
     </>
   );
